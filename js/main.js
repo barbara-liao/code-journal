44428a59025c62a0bc976d4f96e3ff0bd39a2ac0
Barbara Liao <barbara.liao3@gmail.com>
2021-09-08 10:29:38 -0700

    Verify new entries are saved to local storage along with the rest of the data model on page reload and that each entry receives an incrementing entryId. #1.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
 /* global data */
 /* exported data */
+
+function handleInput(event) {
+  $image.src = event.target.value;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var newEntry = {};
+  var newObj = {
+    title: $entryForm.elements.title.value,
+    imageURL: $entryForm.elements.photourl.value,
+    notes: $entryForm.elements.notes.value
+  };
+  for (var i in newObj) {
+    newEntry[i] = newObj[i];
+  }
+  newEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newEntry);
+
+  $image.src = 'images/placeholder-image-square.jpg';
+  $entryForm.reset();
+}
+
+var $photoInput = document.getElementById('photo-input');
+var $image = document.querySelector('img');
+var $entryForm = document.querySelector('#create-form');
+
+$photoInput.addEventListener('input', handleInput);
+$entryForm.addEventListener('submit', handleSubmit);
