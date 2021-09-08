7ae4f8f249ce89f8e23e344ee502383133d581d7
Barbara Liao <barbara.liao3@gmail.com>
2021-09-08 12:00:04 -0700

    Moved media query to bottom, removed unnecessary div tag, removed unnecessary JS object, updated placement of global variables and event listeners to the top of document. #1.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 /* global data */
 /* exported data */
 
-function handleInput(event) {
-  $image.src = event.target.value;
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  var newEntry = {};
-  var newObj = {
-    title: $entryForm.elements.title.value,
-    imageURL: $entryForm.elements.photourl.value,
-    notes: $entryForm.elements.notes.value
-  };
-  for (var i in newObj) {
-    newEntry[i] = newObj[i];
-  }
-  newEntry.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(newEntry);
-
-  $image.src = 'images/placeholder-image-square.jpg';
-  $entryForm.reset();
-}
-
 var $photoInput = document.getElementById('photo-input');
 var $image = document.querySelector('img');
 var $entryForm = document.querySelector('#create-form');
 
 $photoInput.addEventListener('input', handleInput);
 $entryForm.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  $image.src = event.target.value;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var newObj = {
+    title: $entryForm.elements.title.value,
+    imageURL: $entryForm.elements.photourl.value,
+    notes: $entryForm.elements.notes.value
+  };
+  // for (var i in newObj) {
+  //   newEntry[i] = newObj[i];
+  // }
+  newObj.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newObj);
+
+  $image.src = 'images/placeholder-image-square.jpg';
+  $entryForm.reset();
+}
